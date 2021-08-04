a50aef21e6634799e42002269f96ecb410e0ee41
GeloUno <geloke59@gmail.com>
2021-08-04 16:37:39 +0200

    Guard session in client side

--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,7 +1,29 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css'
+import { getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+
 
 function UserProfile() {
+
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        getSession().then(session => {
+            setIsLoading(false)
+            if (!session) {
+                window.location.href = "/auth"
+            }
+        })
+        return () => {
+        }
+    }, [])
+
+
+    if (isLoading) {
+        return <p className={classes.profile}>loading...</p>
+    }
+
     return (
         <section className={classes.profile}>
             <h1>Your user profile</h1>
