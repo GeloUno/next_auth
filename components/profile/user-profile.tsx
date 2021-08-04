1e849a0028cfdc5a45ea818b1b9d74de55067a15
GeloUno <geloke59@gmail.com>
2021-08-04 16:52:19 +0200

    Guard by server side

--- components/profile/user-profile.tsx
+++ components/profile/user-profile.tsx
@@ -1,28 +1,7 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css'
-import { getSession } from 'next-auth/client';
-import { useEffect, useState } from 'react';
-
 
 function UserProfile() {
-
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-
-    useEffect(() => {
-        getSession().then(session => {
-            setIsLoading(false)
-            if (!session) {
-                window.location.href = "/auth"
-            }
-        })
-        return () => {
-        }
-    }, [])
-
-
-    if (isLoading) {
-        return <p className={classes.profile}>loading...</p>
-    }
 
     return (
         <section className={classes.profile}>
@@ -33,3 +12,4 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,4 +1,6 @@
 import UserProfile from '../components/profile/user-profile';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/client';
 
 function profilePage() {
     return (
@@ -7,3 +9,23 @@ function profilePage() {
 }
 
 export default profilePage;
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+    const session = await getSession({ req: context.req })
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/auth",
+                permanent: false
+            }
+        }
+    }
+
+
+    return {
+        props: { session }
+    }
+}
