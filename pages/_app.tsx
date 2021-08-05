faf62999c4be0c995d7e9736539549f58f285372
GeloUno <geloke59@gmail.com>
2021-08-05 17:00:10 +0200

    MyApp add  provider from next auth client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import LayoutPage from '../layout/layout'
+import { Provider } from 'next-auth/client'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <LayoutPage>
-      <Component {...pageProps} />
-    </LayoutPage>
+    <Provider session={pageProps?.session} >
+      <LayoutPage>
+        <Component {...pageProps} />
+      </LayoutPage>
+    </Provider>
   )
 }
 export default MyApp
