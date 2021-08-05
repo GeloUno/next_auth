b3954a447399de78318e3e72e994032adc2c8505
GeloUno <geloke59@gmail.com>
2021-08-05 16:59:32 +0200

    add guard auth pages

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,44 @@
 import AuthForm from '../components/auth/auth-form';
-function authPages() {
+import { useRouter } from 'next/router';
+import { getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+import ScaleLoader from 'react-spinners/ScaleLoader'
+import CenterContainer from '../containers/center';
+
+
+function AuthPages() {
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    const router = useRouter()
+
+    useEffect(() => {
+        getSession().then(session => {
+            if (session) {
+                router.replace("/profile")
+            } else {
+                setIsLoading(false)
+            }
+        })
+        return () => {
+
+        }
+    }, [router])
+
+
+    if (isLoading) {
+        return (
+            <CenterContainer>
+                <ScaleLoader height={50} width={4} margin={8} color={'hsl(280, 60%, 50%)'} />
+            </CenterContainer>
+
+        )
+    }
+
+
     return (
         <AuthForm />
     );
 }
 
-export default authPages;
+export default AuthPages;
